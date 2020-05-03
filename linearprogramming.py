081788efc0003657f5b794868e3b29438003ef3b
DryMemoG <memo29sep@gmail.com>
2020-05-03 17:40:57 -0600

    Graphic solution
    This update contains the graphic solution for every problem with  2 decision variables and n restrictions NO INPUT VARIABLES

--- linearprogramming.py
+++ linearprogramming.py
@@ -1,19 +1,29 @@
 from pylab import *
-
+import numpy as np
 import matplotlib.pyplot as plt 
-#x1 será la primera restriccion 
-#x2 la segunda que depende de x1 = n y x2=0 
+#dibujamos los límites del plano cartesiano
+plt.axhline(0,color="black")
+plt.axvline(0,color="black")
+#matriz que tendrá las variables de decisión y las restricciones
+matriz = np.matrix([[1,0,4],[0,2,12],[3,2,18]] )
+for i in range(3):
+    x =matriz[i,2]/matriz[i,0]
+    y =matriz[i,2]/matriz[i,1]
+    xplot=[x,0]
+    yplot=[0,y]
+    plot(xplot,yplot,color="c")
+show()    
 
+"""
 x1=6
 y1=4
 x2=[8,0]
 y2=[0,6]
 
-plt.axhline(0,color="black")
-plt.axvline(0,color="black")
+
 plt.xlim(-0.5,x1+5)
 plt.ylim(-0.5,y1+5)
 plt.axhline(y=y1,xmin=0,xmax=x1+5,color="blue")
 plt.axvline(x=x1,ymin=0,ymax=y1+5,color="red")
 plot(x2,y2,color="green")
-show()
+show()"""
