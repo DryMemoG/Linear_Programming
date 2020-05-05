7843c4cc013a848026b7cf0447391e057b117c05
DryMemoG <memo29sep@gmail.com>
2020-05-05 14:11:22 -0600

    Graphics Solved
    Every graph in the options to input are solved now

--- linearprogramming.py
+++ linearprogramming.py
@@ -6,20 +6,45 @@ import matplotlib.pyplot as plt
 plt.axhline(0,color="black")
 plt.axvline(0,color="black")
 #z = Ax+By
+b=0 #bandera del array para la cuenta 
+parejas=[]
+color = "b"
 #matriz que tendrá las variables de decisión y las restricciones
-matriz = np.matrix([[1,0,4],[0,2,12],[3,2,18]] ) #matriz de 3xn donde se almacenan las restricciones
+matriz = np.matrix([[5,15,50],[20,5,40],[15,2,60]] ) #matriz de 3xn donde se almacenan las restricciones
 for i in range(3): #range(n) donde n son las restricciones 
     if(matriz[i,0]==0): #en caso tengamos una f(x)=n
         temp = matriz[i,2]/matriz[i,1] #temp dividirá los valores para las gráficas
+        parejas.append('largo')
         hlines(y=temp,xmin=0,xmax=10,color="b")  #como f(x)=n es una linea vertical u horizontal dibujamos la línea
-    x =matriz[i,2]/matriz[i,0] #aquí si no se cumple nada de lo anterior dividimos normalmente 
+        xplot=[0,0]#establecemos X y Y como parejas cartesianas
+        
+    else:
+        x=matriz[i,2]/matriz[i,0] #aquí si no se cumple nada de lo anterior dividimos normalmente 
+        parejas.append(str(x))
+        xplot=[x,0]#establecemos X y Y como parejas cartesianas
+        
+    
     if(matriz[i,1]==0):
         temp = matriz[i,2]/matriz[i,0]
+        parejas.append('largo')
         vlines(x=temp,ymin=0,ymax=10,color="g")
-    y=matriz[i,2]/matriz[i,1]
-    xplot=[x,0]#establecemos X y Y como parejas cartesianas
-    yplot=[0,y]
-    plot(xplot,yplot,color="c") #ploteamos la/s linea/s 
+        yplot=[0,0]
+        
+    else:
+        y=matriz[i,2]/matriz[i,1]
+        parejas.append(str(y))
+        yplot=[0,y]
+        
+    
+    
+    plot(xplot,yplot,color="r") #ploteamos la/s linea/s 
+    plt.axhline(0,color="black")
+    plt.axvline(0,color="black")
+for i in parejas:
+    print(i,',')
+    
+    
+    
     
     
 show()    
