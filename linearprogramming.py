0523d3335750c0e2a4f7f62af27d4251afdf3cdd
DryMemoG <memo29sep@gmail.com>
2020-05-06 16:16:04 -0600

    Solution by graphical method
    the software now solves via graphical method and displays the most viable solutions area

--- linearprogramming.py
+++ linearprogramming.py
@@ -2,19 +2,24 @@
 from pylab import *
 import numpy as np
 import matplotlib.pyplot as plt 
+
 #dibujamos los límites del plano cartesiano
 plt.axhline(0,color="black")
 plt.axvline(0,color="black")
-zA =4
-zB=2
+#variables de Z
+zA=0.4
+zB=0.5
+
 b=0 #bandera del array para la cuenta 
+leyenda=""
 parejas=[]
 ecuaciones =[]
 constantes=[]
 color = "b"
 rango =3
 #matriz que tendrá las variables de decisión y las restricciones
-matriz = np.matrix([[5,15,50],[20,5,40],[15,2,60]] ) #matriz de 3xn donde se almacenan las restricciones
+matriz = np.matrix([[0.3,0.1,2.7],[0.5,0.5,6],[0.6,0.4,6]] ) #matriz de 3xn donde se almacenan las restricciones
+restricciones=('0','1','2',) #donde (0,<)(1,=)(2,>)
 for i in range(rango): #range(n) donde n son las restricciones 
     tpx=0
     tpy=0
@@ -23,13 +28,14 @@ for i in range(rango): #range(n) donde n son las restricciones
     bipx=-1
     bipy=-1
     if(matriz[i,0]==0): #en caso tengamos una f(x)=n
-        temp = matriz[i,2]/matriz[i,1] #temp dividirá los valores para las gráficas
-        parejas.append('10')
-        hlines(y=temp,xmin=0,xmax=10,color="b")  #como f(x)=n es una linea vertical u horizontal dibujamos la línea
+        temp = matriz[i,2]/matriz[i,1] #temp dividirá los valores para las gráficas ymaxima en funcion constante
+        parejas.append(matriz[i,2])
+        hlines(y=temp,xmin=0,xmax=matriz[i,2],color="b")  #como f(x)=n es una linea vertical u horizontal dibujamos la línea
         xplot=[0,0]#establecemos X y Y como parejas cartesianas
         tpx=temp
         bipx=1 #bandera para puntos de intersección
         constantes.append(str(tpx))
+        
         
     else:
         x=matriz[i,2]/matriz[i,0] #aquí si no se cumple nada de lo anterior dividimos normalmente 
@@ -40,11 +46,12 @@ for i in range(rango): #range(n) donde n son las restricciones
         
     if(matriz[i,1]==0):
         temp = matriz[i,2]/matriz[i,0]
-        parejas.append('10')
-        vlines(x=temp,ymin=0,ymax=10,color="g")
+        parejas.append(matriz[i,2])
+        vlines(x=temp,ymin=0,ymax=matriz[i,2],color="g")
         yplot=[0,0]
         tpy=temp
         bipy=1
+        
         constantes.append(str(tpy))
         
         
@@ -55,28 +62,52 @@ for i in range(rango): #range(n) donde n son las restricciones
         tpy=y
         yy1=0
     plt.title("Solucion por Método Gráfico")
+    
     if xx1==0:    
         plot(tpx,0,'ro')
         z=((zA*float(tpx))+0)
-        print("(x1,x2)="+"("+str(round(tpx,2))+","+str(round(0,2))+") con Z="+str(round(z,2)))
+        #condiciones para las soluciones
+        if restricciones[i]=='0':
+            if z<=matriz[i,2]:
+                leyenda=("(x1,x2)="+"("+str(round(tpx,2))+","+str(round(0,2))+") con Z="+str(round(z,2)))
+        elif restricciones[i]=='1':
+            if z==matriz[i,2]:
+                leyenda=("(x1,x2)="+"("+str(round(tpx,2))+","+str(round(0,2))+") con Z="+str(round(z,2)))
+        elif restricciones[i]=='2':
+            if z >= matriz[i,2]:
+                leyenda=("(x1,x2)="+"("+str(round(tpx,2))+","+str(round(0,2))+") con Z="+str(round(z,2)))
+        
         punto='('+str(round(tpx,2))+','+str(round(0,2))+')'
         plt.annotate(punto,(tpx,0),fontsize="x-small")
+        #plot(0,0,color="white",label=leyenda)#mandamos la respuesta a una leyenda en la esquina superior izquierda
+        #legend(loc='upper-left',title="Área de Soluciones")
         
     if yy1==0:
         plot(0,tpy,'ro')
         z=(0+(zB*float(tpy)))
-        print("(x1,x2)="+"("+str(round(0,2))+","+str(round(tpy,2))+") con Z="+str(round(z,2)))
+        if restricciones[i]=='0':
+            if z<=matriz[i,2]:
+                leyenda=("(x1,x2)="+"("+str(round(0,2))+","+str(round(tpy,2))+") con Z="+str(round(z,2)))
+        elif restricciones[i]=='1':
+            if z==matriz[i,2]:
+                leyenda=("(x1,x2)="+"("+str(round(0,2))+","+str(round(tpy,2))+") con Z="+str(round(z,2)))
+        elif restricciones[i]=='2':
+            if z >= matriz[i,2]:
+                leyenda=("(x1,x2)="+"("+str(round(0,2))+","+str(round(tpy,2))+") con Z="+str(round(z,2)))
+        
         punto='('+str(round(0,2))+','+str(round(tpy,2))+')'
         plt.annotate(punto,(0,tpy),fontsize="x-small")
+       # plot(0,0,color="white",label=leyenda)#mandamos la respuesta a una leyenda en la esquina superior izquierda
+       # legend(loc='upper-left',title="Área de Soluciones")
         
         
     plot(tpx,tpy,0)
     
-    plot(xplot,yplot,color="r", label="ecuacion") #ploteamos la/s linea/s 
+    plot(xplot,yplot,color="r") #ploteamos la/s linea/s 
     plt.axhline(0,color="black")
     plt.axvline(0,color="black")
 #ecuaciones de las rectas
-print(parejas)
+
 for x in range(0,(rango*2),2):
     frmX=float(parejas[x])
     frmY=float(parejas[x+1])
@@ -96,16 +127,21 @@ for x in range(0,(rango*2),2):
         
         mx=((float(ecuaciones[x])*-1)+(float(ecuaciones[y])))
         B=((float(ecuaciones[x+1])+(float(ecuaciones[y+1])*-1)))
-        ipx= (B/mx)
+        if mx==0:
+            ipx=0
+        else:
+            ipx= (B/mx)
         ipy=((float(ecuaciones[x])*float(ipx))+float(ecuaciones[x+1]))
         
-        
-        if ipx >0 and ipy >0 and bipx<0 and bipy<0:
+        #calculos de intersección
+        if ipx >0 and ipy >0: #and bipx<0 and bipy<0:
             plot(ipx,ipy,'ro')
             punto='('+str(round(ipx,2))+','+str(round(ipy,2))+')'
             plt.annotate(punto,(ipx,ipy),fontsize="x-small")
             z=((zA*float(ipx))+(zB*float(ipy)))
-            print("(x1,x2)="+"("+str(round(ipx,2))+","+str(round(ipy,2))+") con Z="+str(round(z,2)))
+            leyenda=("(x1,x2)="+"("+str(round(ipx,2))+","+str(round(ipy,2))+") con Z="+str(round(z,2)))#respuestas
+            plot(0,0,color="white",label=leyenda)
+            legend(loc='upper-left',title="Área de Soluciones")
         
 
     
